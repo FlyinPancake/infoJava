594c7d25d1e5ebff1a557dcc6396d3d26e24b8c0
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2020-11-03 12:43:22 +0100

    lab7 added

---
+++ lab7/feladat_5/swingmvclab/StudentData.java
@@ -0,0 +1,86 @@
+﻿package swingmvclab;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+/*
+ * A hallgatók adatait tároló osztály.
+ */
+public class StudentData extends AbstractTableModel{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3447886449961442248L;
+    /*
+     * Ez a tagváltozó tárolja a hallgatói adatokat. NE MÓDOSÍTSD!
+     */
+    List<Student> students = new ArrayList<Student>();
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public int getRowCount() {
+        return students.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Student student = students.get(rowIndex);
+        switch(columnIndex) {
+            case 0: return student.getName();
+            case 1: return student.getNeptun();
+            case 2: return student.hasSignature();
+            default: return student.getGrade();
+        }        
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        String[] columnNames = 
+        {"Név",
+         "Neptun", 
+         "Aláírás", 
+         "Jegy"};
+        return columnNames[column];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        Class<?>[] columnClasses = {String.class, String.class, Boolean.class, Integer.class};
+        return columnClasses[columnIndex];
+        
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        ArrayList<Integer> editableColumns = new ArrayList<Integer>(Arrays.asList(new Integer[] {2,3}));
+        if (editableColumns.contains(columnIndex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        students.get(rowIndex);
+        switch (columnIndex){
+            case 2: students.get(rowIndex).setSignature((Boolean)aValue);
+                break;
+        
+            default: students.get(rowIndex).setGrade((Integer)aValue);;
+                break;
+        }
+    }
+
+    public void addStudent(String name, String neptun) {
+        students.add(new Student(name, neptun, false, 0));
+    }
+}
---
+++ lab7/feladat_7/swingmvclab/StudentFrame.java
@@ -0,0 +1,164 @@
+package swingmvclab;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
+/*
+ * A megjelenítendő ablakunk osztálya.
+ */
+public class StudentFrame extends JFrame {
+    
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3769575128566519194L;
+    /*
+     * Ebben az objektumban vannak a hallgatói adatok. A program indulás után
+     * betölti az adatokat fájlból, bezáráskor pedig kimenti oda.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    private StudentData data;
+
+
+    private JTextField neptunField;
+    private JTextField nameField;
+
+    /*
+     * Itt hozzuk létre és adjuk hozzá az ablakunkhoz a különböző komponenseket
+     * (táblázat, beviteli mező, gomb).
+     */
+    private void initComponents() {
+        this.setLayout(new BorderLayout());
+        JTable table = new JTable(data);
+        table.setFillsViewportHeight(true);
+        //table.setAutoCreateRowSorter(true);
+        TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(table.getModel());
+        table.setRowSorter(sorter);
+        table.setDefaultRenderer(Boolean.class, new StudentTableCellRenderer(table.getDefaultRenderer(Boolean.class)));
+        table.setDefaultRenderer(String.class, new StudentTableCellRenderer(table.getDefaultRenderer(String.class)));
+        table.setDefaultRenderer(Integer.class, new StudentTableCellRenderer(table.getDefaultRenderer(Integer.class)));
+        JScrollPane scrollPane = new JScrollPane(table);
+        this.add(scrollPane,BorderLayout.CENTER);
+
+        JPanel addRowPanel = new JPanel();
+        nameField = new JTextField(20);
+        addRowPanel.add(new JLabel("Név: "));
+        addRowPanel.add(nameField);
+        neptunField = new JTextField(6);
+        addRowPanel.add(new JLabel("Neptun"));
+        addRowPanel.add(neptunField);
+
+        JButton addRowButton = new JButton("Felvesz");
+        addRowButton.addActionListener(new AddButtonActionListener());
+        addRowPanel.add(addRowButton);
+
+        this.add(addRowPanel,BorderLayout.SOUTH);
+
+    }
+    class AddButtonActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            data.addStudent(nameField.getText(), neptunField.getText());
+            data.fireTableRowsInserted(data.students.size()-1, data.students.size()-1);
+        }
+    }
+
+    class StudentTableCellRenderer implements TableCellRenderer {
+        private TableCellRenderer renderer;
+
+        public StudentTableCellRenderer(TableCellRenderer defRenderer) {
+                this.renderer = defRenderer;
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            Component component = renderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            // Kikeressük az éppen megjelenítendő hallgatót a külső,
+            // StudentFrame osztály data tagváltozójából,
+            // megállapítjuk, hogy bukásra áll-e vagy sem,
+            // és ez alapján átállítjuk a komponens háttérszínét:
+            // component.setBackground(...)
+            
+            Student student = data.students.get(table.getRowSorter().convertRowIndexToModel(row));
+            if (student.hasSignature() && student.getGrade() >= 2) {
+                component.setBackground(Color.GREEN);
+            } else {
+                component.setBackground(Color.RED);
+            }
+            return component;
+        }
+    }
+    
+
+    /*
+     * Az ablak konstruktora.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    @SuppressWarnings("unchecked")
+    public StudentFrame() {
+        super("Hallgatói nyilvántartás");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        // Induláskor betöltjük az adatokat
+        try {
+            data = new StudentData();
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("students.dat"));
+            data.students = (List<Student>)ois.readObject();
+            ois.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        // Bezáráskor mentjük az adatokat
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("students.dat"));
+                    oos.writeObject(data.students);
+                    oos.close();
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        // Felépítjük az ablakot
+        setMinimumSize(new Dimension(500, 200));
+        initComponents();
+    }
+
+    /*
+     * A program belépési pontja.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    public static void main(String[] args) {
+        // Megjelenítjük az ablakot
+        StudentFrame sf = new StudentFrame();
+        sf.setVisible(true);
+    }
+}
