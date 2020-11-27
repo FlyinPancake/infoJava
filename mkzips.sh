4e839f4a66f21156fe742a18dfbb1e8ed179f969
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2020-11-27 08:26:36 +0100

    zipmaker working

--- mkzips.sh
+++ mkzips.sh
@@ -1,9 +1,25 @@
-if ("$1" == "")
-then
-	echo "not implemented"
-else
-	mkdir zips
-	cd "lab$1"
-	zip	"../zips/lab$1".zip -r *
+if [ $# -eq 0 ]; then
+    echo "Nincs labor megadva"
+    echo "Help: -h"
+    exit 1
 fi
 
+if [ "$*" == "-h" ]; then
+	echo "sima labor: ./mkzips.sh 'labor szama'"
+	echo "imsc labor: ./mkzips.sh -i 'labor szama'"
+	echo ""
+	exit 0
+fi
+
+LAB=$1
+
+if [ "$1" == "-i" ]; then
+	LAB=$2"_imsc"
+	echo $LAB
+	exit 0
+fi
+
+mkdir -p zips
+cd "lab$LAB"
+zip	"../zips/lab$LAB".zip -r * >> /dev/null
+echo "Zipfile created in zips/lab$LAB.zip"
